c4b6f00130e4761fbc1e7b480053cddc29aaab19
eduarchilon <eduarchilon@outlook.es>
2023-10-16 12:27:26 -0300

    v6

--- app.js
+++ app.js
@@ -4,17 +4,12 @@ const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
 const sharp = require("sharp");
-const { Octokit } = require("@octokit/rest");
 
 const app = express();
 const port = 3000;
 app.use(cors());
 
 app.use(express.json());
-
-const octokit = new Octokit({
-  auth: "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", // Asegúrate de definir esta variable de entorno
-});
 
 // Configuración de Multer para el almacenamiento de archivos
 const storage = multer.diskStorage({
@@ -30,12 +25,12 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // Ruta para subir o reemplazar una imagen
-app.post("/upload/:userId", upload.single("image"), async (req, res) => {
+app.post("/upload/:userId", upload.single("image"), (req, res) => {
   const userId = req.params.userId;
   const imagePathPNG = path.join(__dirname, "uploads", userId + ".png");
 
   // Convierte el archivo de imagen al formato PNG directamente
-  sharp(req.file.path).toFile(imagePathPNG, async (conversionError) => {
+  sharp(req.file.path).toFile(imagePathPNG, (conversionError) => {
     if (conversionError) {
       console.error(
         "Error al convertir y guardar la imagen en formato PNG",
@@ -48,34 +43,11 @@ app.post("/upload/:userId", upload.single("image"), async (req, res) => {
         if (deleteError) {
           console.error("Error al eliminar el archivo original", deleteError);
         }
-        // Subir la imagen a GitHub
-        uploadImageToGitHub(userId, imagePathPNG)
-          .then(() => {
-            res.send("Imagen subida y convertida a PNG correctamente");
-          })
-          .catch((uploadError) => {
-            console.error("Error al subir la imagen a GitHub", uploadError);
-            res.status(500).send("Error al subir la imagen a GitHub");
-          });
+        res.send("Imagen subida y convertida a PNG correctamente");
       });
     }
   });
 });
-
-async function uploadImageToGitHub(userId, imagePath) {
-  try {
-    const fileContent = fs.readFileSync(imagePath);
-    await octokit.repos.createOrUpdateFile({
-      owner: "eduarchilon",
-      repo: "avatar-api",
-      path: `uploads/${userId}.png`,
-      message: `Subida de imagen para ${userId}`,
-      content: fileContent.toString("base64"),
-    });
-  } catch (error) {
-    throw error;
-  }
-}
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
